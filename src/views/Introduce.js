07f451e5ae6c19ed5015866ceec0d4d9e6fa41c8
kent99821 <2270514313@qq.com>
2021-10-21 15:46:02 +0800

    完成团队简介

--- src/views/Introduce.js
+++ src/views/Introduce.js
@@ -1,14 +1,46 @@
-import { Row, Col } from "antd";
-import React from "react";
+import React,{ useState, useEffect } from "react";
+import { Row, Col, message, Card } from "antd";
+import axios from "axios";
+import '../assets/css/Introduce.css';
+import apiUrl from "../conf/apiUrl";
 function Introduce(){
+    const [introduction, setIntroduction] = useState({});
+    const [show, setShow] = useState(false);
+    const getIntroduce = ()=>{
+        axios({
+            method:'get',
+            url: apiUrl.introduce,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            withCredentials: true
+        }).then(
+            res =>{
+                setIntroduction(res.data.data[0]);
+                setShow(true);
+            })
+    }
+    useEffect(() => {
+        getIntroduce();
+    }, [])
+    if (!show) {
+        return (
+            message.loading('资源加载中', 2)
+        )
+    }
     return(
         <div>
             <Row>
-                <Col span={12}>
-                <div>简介</div>
+            <Col xs={24} sm={12} md={12} lg={12} xl={12} xxl={12}>
+                <div className="introduce">
+                    <h1 className="title">图灵智能创新团队介绍</h1>
+                        <p className="content">{introduction.introductionInfo}</p>
+                        <br/>
+                        <p className="date">更新日期: {introduction.introductionEditTime}</p>
+                </div>
                 </Col>
-                <Col span={12}>
-                    <video src="" controls="controls"></video>
+                <Col xs={24} sm={12} md={12} lg={12} xl={12} xxl={12} >
+                    <div className="introduce">
+                    <video className="video" src="http://150.158.171.105:9876/static/2021Video.mp4" controls="controls"></video>
+                    </div>
                 </Col>
             </Row>
         </div>

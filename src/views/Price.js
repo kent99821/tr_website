e534fc0d1e9e2b4ddbf6d327b29edf66e0c9766b
kent99821 <2270514313@qq.com>
2021-10-24 15:01:59 +0800

    完成团队荣誉模块

--- src/views/Price.js
+++ src/views/Price.js
@@ -1,7 +1,47 @@
-import React from "react";
-function Price(){
-    return(
-        <div>团队荣誉</div>
+import React, { useState, useEffect } from "react";
+import { Row, Col, message, Card } from 'antd';
+import '../assets/css/Leader.css';
+import apiUrl from "../conf/apiUrl";
+import axios from "axios";
+function Price() {
+    const [prices, setPrices] = useState([]);
+    const [show, setShow] = useState(false);
+    const getPrices = () => {
+        axios({
+            method: 'get',
+            url: apiUrl.price,
+            headers: { 'Access-Control-Allow-Origin': '*' },
+            withCredentials: true
+        }).then(
+            res => {
+                setPrices(res.data.data);
+                setShow(true);
+            }
+        )
+    }
+    useEffect(() => {
+        getPrices();
+    }, [])
+    if (!show) {
+        return (
+            message.loading('资源加载中', 2)
+        )
+    }
+    return (
+        <div>
+            <Row type="flex" align="middle" justify="center" >
+                {prices.map(item => (
+                    <Col xs={24} sm={12} md={12} lg={12} xl={6} xxl={6}>
+                        <Card className="Ldcard"
+                            hoverable
+                            cover={
+                                <img className="ldImg" alt="project" src={item.awardPhoto[0].awardPhotoLoc} />}>
+                            <p className="ldTitle">{item.awardName}</p>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </div>
     )
 }
 export default Price;
